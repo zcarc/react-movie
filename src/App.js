591c6fdc908a9504dc587a1ffc8a54a1d7788156
wrkbr <zcarc1@gmail.com>
2019-11-25 01:56:31 +0900

    #2 2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React from 'react';
 
-function Food({favorite}) {
-  return <h1>I like {favorite}</h1>;
+function Food({name, picture}) {
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture}></img>
+
+  </div>;
 }
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg'
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://aromasian.com/wp-content/uploads/2017/07/grill-pork-belly-samgyeopsal.jpg'
+  },
+  {
+    name: 'Bibimbap',
+    image: 'https://www.maangchi.com/wp-content/uploads/2008/01/bibimbap-bowl.jpg'
+  },
+  {
+    name: 'Donkasu',
+    image: 'https://s3-media1.fl.yelpcdn.com/bphoto/5d7cIbC6vpXyvV2SZHrdzw/l.jpg'
+  },
+  {
+    name: 'Kimbap',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Vegetable_gimbap.jpg/1200px-Vegetable_gimbap.jpg'
+  },
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food favorite='kimchi'/>
-      <Food favorite='ramen'/>
-      <Food favorite='samgiopsal'/>
-      <Food favorite='chukumi'/>
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   )
 }
