b4300a9e36a91ee285a262319dd2b80ee044c985
wrkbr <zcarc1@gmail.com>
2019-11-30 04:08:24 +0900

    #5 0 Deploying to Github Pages

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component{
       data: {
         data: {movies}
       }
-    } = await axios.get('http://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
 
     this.setState({movies, isLoading:false});
   }
